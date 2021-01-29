dbedd14b4e871260b863d91a1e6dbd9c7b6f7b71
ifwp <ifwp@vdsoe.com>
2021-01-29 16:14:43 -0600

    IFWP

--- ifwp-front-end-forms.php
+++ ifwp-front-end-forms.php
@@ -19,10 +19,13 @@ if(defined('ABSPATH')){
         wp_enqueue_script('ifwp-floating-labels-b4', plugin_dir_url(__FILE__) . 'ifwp-floating-labels-b4.js', ['jquery'], filemtime(plugin_dir_path(__FILE__) . 'ifwp-floating-labels-b4.js'), true);
     });
     add_action('plugins_loaded', function(){
+        if(!class_exists('Puc_v4_Factory')){
+			require_once(plugin_dir_path(__FILE__) . 'plugin-update-checker-4.10/plugin-update-checker.php');
+		}
+        Puc_v4_Factory::buildUpdateChecker('https://github.com/ifwp/ifwp-front-end-forms', __FILE__, 'ifwp-front-end-forms');
         if(!class_exists('simple_html_dom')){
             require_once(plugin_dir_path(__FILE__) . 'simple-html-dom-1.9.1/simple_html_dom.php');
 		}
+        require_once(plugin_dir_path(__FILE__) . 'mb/ifwp-mb-b4.php');
     });
-    // Meta Box
-    require_once(plugin_dir_path(__FILE__) . 'mb/ifwp-mb-b4.php');
 }
